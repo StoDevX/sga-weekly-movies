f34c204f3714e357860b7c5f2c1c78db0fef111b
Hawken Rives <hawkrives@gmail.com>
2018-02-04 21:38:27 -0600

    add _secrets file for providing api keys to pythonista

--- bin/lib/keys.py
+++ bin/lib/keys.py
@@ -1,19 +1,21 @@
 import os
 import sys
+try:
+    from ._secrets import OMDB_API_KEY, TMDB_API_KEY
+except ModuleNotFoundError:
+    OMDB_API_KEY = os.getenv('OMDB_API_KEY', None)
+    TMDB_API_KEY = os.getenv('TMDB_API_KEY', None)
 
 
-OMDB_API_KEY = os.getenv('OMDB_API_KEY', None)
 if not OMDB_API_KEY:
     print('This tool requires an OMDB api key. '
           'If you do not have one, please visit <http://www.omdbapi.com/apikey.aspx>, '
-          'then `export OMDB_API_KEY="your-key" pipenv run python {sys.argv[0]}`',
+          f'then `export OMDB_API_KEY="your-key" pipenv run python {sys.argv[0]}`',
           file=sys.stderr)
 
-
-TMDB_API_KEY = os.getenv('TMDB_API_KEY', None)
 if not TMDB_API_KEY:
-    print(f'This tool requires a TMDB api key. '
+    print('This tool requires a TMDB api key. '
           'If you do not have one, please visit <https://www.themoviedb.org>, '
           'log in and get the v3 API key, then '
-          '`export TMDB_API_KEY="your-key" pipenv run python {sys.argv[0]}`',
+          f'`export TMDB_API_KEY="your-key" pipenv run python {sys.argv[0]}`',
           file=sys.stderr)
