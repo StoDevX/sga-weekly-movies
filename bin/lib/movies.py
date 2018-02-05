5d3b2d701a16c02000dde4da9c222a25a538ad68
Hawken Rives <hawkrives@gmail.com>
2018-02-04 21:25:57 -0600

    add pythonista-specific movie picker

--- bin/lib/movies.py
+++ bin/lib/movies.py
@@ -1,5 +1,12 @@
 import requests
-from pick import pick
+
+try:
+    import dialogs
+    from ui import ListDataSource
+    is_pythonista = True
+except ModuleNotFoundError:
+    from pick import pick
+    is_pythonista = False
 
 from .keys import OMDB_API_KEY
 
@@ -18,9 +25,21 @@ def find_movie(title, year):
 
     r = requests.get('http://www.omdbapi.com/', params=params)
     results = r.json()
-    options = [f'{m["Title"]} ({m["Year"]}) <{m["Poster"]}>' for m in results['Search']]
 
-    [_, chosen_index] = pick(options)
+    if is_pythonista:
+        options = [{
+            'title': f'{m["Title"]} ({m["Year"]})',
+            'accessory_type': 'disclosure_indicator',
+        } for m in results['Search']]
+
+        chosen_title = dialogs.list_dialog('Pick one', options)
+        if not chosen_title:
+            return None
+
+        chosen_index = options.index(chosen_title)
+    else:
+        options = [f'{m["Title"]} ({m["Year"]})' for m in results['Search']]
+        [chosen_title, chosen_index] = pick(options)
 
     chosen_movie = results['Search'][chosen_index]
 
